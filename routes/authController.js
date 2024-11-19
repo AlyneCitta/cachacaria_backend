c22db613522a534d145010da2f7c9f3a99316276
Alyne Citta <alyne_fae@estudante.sesisenai.org.br>
2024-11-19 14:02:21 -0300

    backend auntenticação de usuario

---
+++ routes/authController.js
@@ -0,0 +1,37 @@
+const express = require('express'); 
+const router = express.Router();
+const pool = require('../db');
+const jwt = require('jsonwebtoken');
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const client = await pool.connect();
+
+        // Verifica o usuário na tabela 'usuario'
+        const user = await client.query(
+            'SELECT * FROM usuario WHERE email = $1 AND senha = $2', 
+            [email, password]
+        );
+
+        if (user.rows.length > 0) {
+            const { id, email } = user.rows[0];
+            const role = 'user'; // Define uma função padrão, pode ser expandido futuramente
+            const token = jwt.sign({ id, email, role }, process.env.SECRET, { expiresIn: 600 });
+            client.release();
+            return res.json({ auth: true, token, role });
+        }
+
+        client.release();
+        return res.status(401).json({ auth: false, message: 'Usuário não encontrado ou credenciais inválidas.' });
+    } catch (err) {
+        console.error('Erro ao executar a consulta', err);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+router.post('/logout', async (req, res) => {
+    res.json({ auth: false, token: null });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require("dotenv-safe").config();
+
 const app = express();
 const port = 3001;
 
+const authRouter = require('./routes/authController');
 const userRoutes = require('./routes/userRoutes');
 const cityRoutes = require('./routes/cityRoutes'); 
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+function verifyJWT(req, res, next) {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ auth: false, message: 'No token provided.' });
+    }
+
+    const tokenWithoutBearer = token.replace('Bearer ', '');
+
+    jwt.verify(tokenWithoutBearer, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            console.error('Erro ao verificar token:', err);
+            return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+        }
+
+        // Anexar informações do usuário ao objeto de requisição
+        req.userId = decoded.id;
+        req.userRole = decoded.role; // Role incluída no payload do token
+        next();
+    });
+}
+
+app.get('/', verifyJWT, (req, res) => {
     res.send('Servidor está funcionando!');
 });
 
+app.use('/auth', authRouter);
 app.use('/api/users', userRoutes);
 app.use('/api/cities', cityRoutes); 
 
